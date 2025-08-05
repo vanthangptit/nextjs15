47cdeb8531d3d4e7aa863d2dd41a509657ad7a66
thangnv <thang.nguyen@irsvietnam.com>
2025-08-05 15:50:18 +0700

    - Update the pm2 and config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'dnApp',
-      script: './.next/standalone/server.js',
+      script: 'node_modules/next/dist/bin/next',
+      cwd: './',
+      watch: ['./src'],
       output: './logs/app.log',
       error: './logs/app.err',
       log_date_format: 'DD-MM-YYYY',
-      args: 'start -p 3000',
-      watch: ['./src'],
-      cwd: './',
       env: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT: 3000
       }
     }
   ]
