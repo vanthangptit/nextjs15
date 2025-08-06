7f684915f29391cfbb739166dae5233339d9afab
thangnv <thang.nguyen@irsvietnam.com>
2025-08-06 11:31:37 +0700

    - Remove the env property from the PM2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,7 @@ module.exports = {
       watch: ['./src'],
       output: './logs/app.log',
       error: './logs/app.err',
-      log_date_format: 'DD-MM-YYYY',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000
-      }
+      log_date_format: 'DD-MM-YYYY HH:mm'
     }
   ]
 };
