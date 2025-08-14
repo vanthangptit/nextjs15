8ddb1f1b0e6d6ad1077f4256798e2d3f46eab784
thangnv <thang.nguyen@irsvietnam.com>
2025-08-14 14:20:38 +0700

    -Down memory ram on ec2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_APP_NAME || 'dnApp',
-      script: 'node_modules/next/dist/bin/next',
+      script: 'node',
+      args: '--max-old-space-size=512 ./node_modules/next/dist/bin/next start',
       watch: false,
       output: './logs/app.log',
       error: './logs/app.err',
