2e6e261e4840b84ec743c2d33b3f033fafdc26ef
thangnv <thang.nguyen@irsvietnam.com>
2025-08-14 08:37:56 +0700

    - Removing the current working directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: process.env.PM2_APP_NAME || 'dnApp',
       script: '.next/standalone/server.js',
-      cwd: __dirname,
       watch: false,
       output: './logs/app.log',
       error: './logs/app.err',
